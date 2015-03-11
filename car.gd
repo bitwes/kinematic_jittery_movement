14f4af554fb01c3c1523b0b37a75bc883ca3d91c
bitwes <butch.wesley@gmail.com>
2015-03-11 19:17:51 -0400

    Fixed the checking x instead of y to release.

--- car.gd
+++ car.gd
@@ -7,10 +7,9 @@ func _ready():
 	set_fixed_process(true)
 
 func _fixed_process(delta):
+	
 	move(Vector2(speed * delta, 0))
 	
-	if(get_pos().y > 1000):
-		queue_free()
-	
+	if(get_pos().x > 1000):
 
 
